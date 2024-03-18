723bb842131d5a82b651e3de7805494b46875fbc
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2024-03-18 21:33:53 +0000

    Debug Project schema generator

---
+++ JsonSchemaGenerator/Options.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+
+namespace JsonSchemaGenerator;
+
+internal class Options
+{
+    [Option("output", Default = false, Required = true)]
+    public string Output { get; set; } = "";
+}
---
+++ JsonSchemaGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using BitMagic.X16Debugger;
+using CommandLine;
+using JsonSchemaGenerator;
+using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Serialization;
+
+Console.WriteLine("BitMagic - Debug Project Schema Generator");
+
+ParserResult<Options>? argumentsResult;
+try
+{
+    argumentsResult = Parser.Default.ParseArguments<Options>(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error processing arguments:");
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+var options = argumentsResult?.Value ?? throw new Exception();
+
+var generator = new JSchemaGenerator();
+generator.ContractResolver = new CamelCasePropertyNamesContractResolver();
+generator.SchemaIdGenerationHandling = SchemaIdGenerationHandling.TypeName;
+generator.SchemaReferenceHandling = SchemaReferenceHandling.All;
+generator.SchemaLocationHandling = SchemaLocationHandling.Inline;
+
+var schema = generator.Generate(typeof(X16DebugProject));
+
+var filename = Path.GetFullPath(options.Output);
+
+Console.WriteLine($"Writing to : {filename}");
+File.WriteAllText(filename, schema.ToString());
+Console.WriteLine("Done.");
