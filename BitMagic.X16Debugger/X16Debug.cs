ea8a9655176999f635a3c9318ea50d9ec49538b4
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2025-12-16 18:01:17 +0000

    Fix stepping on stack breakpoints

--- BitMagic.X16Debugger/X16Debug.cs
+++ BitMagic.X16Debugger/X16Debug.cs
@@ -1379,7 +1379,7 @@ public class X16Debug : DebugAdapterBase
                 return;
             }
         }
-
+        _emulator.Stepping = true;
         if ((_emulator.State.StackBreakpointHit ^ DebugConstants.SystemBreakpoint) == 0) // this is just a debugger breakpoint, so continue
         {
             _emulator.Stepping = false;
